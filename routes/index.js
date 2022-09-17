c025339bbc6b633a29719f852ab2ec7f73ce3f84
Christian Engel <ch-engel@posteo.de>
2022-09-17 13:51:17 +0200

    Allow base path configuration

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const { config } = require("dotenv");
 const router = express.Router();
 
+config();
+
 const INDEX_CONFIG = {
+  BASE: process.env.BASE || "",
   STYLE_URL:
     process.env.STYLE_URL ||
     "http://localhost:8080/tiles/styles/osm-bright-gl-style/style.json",
+  title: "Wie weit zum Altglas",
 };
 
 /* GET home page. */
