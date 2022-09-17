9c46d07b64d746b0bf2d989fa430f2c40516473f
Christian Engel <ch-engel@posteo.de>
2022-09-17 14:18:11 +0200

    Add base path to static files

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 config();
 
+const STATIC_BASE = process.env.BASE === "/" ? "" : process.env.BASE;
 const INDEX_CONFIG = {
   BASE: process.env.BASE || "",
+  STATIC_BASE: STATIC_BASE,
   STYLE_URL:
     process.env.STYLE_URL ||
     "http://localhost:8080/tiles/styles/osm-bright-gl-style/style.json",
