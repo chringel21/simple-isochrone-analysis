a133777546d743315df64d2636a09a97a2a039db
Christian Engel <ch-engel@posteo.de>
2022-09-19 10:48:00 +0200

    Use actual base env variable

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const app = express();
 
 config();
 
+const STATIC_BASE = process.env.BASE === "/" ? "" : process.env.BASE;
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -21,11 +23,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(`${process.env.BASE}`, express.static(path.join(__dirname, "public")));
+app.use(`${STATIC_BASE}`, express.static(path.join(__dirname, "public")));
 
-app.use(`${process.env.BASE}`, indexRouter);
-app.use(`${process.env.BASE}/points`, pointsRouter);
-app.use(`${process.env.BASE}/isochrone`, isochroneRouter);
+app.use(`${STATIC_BASE}/`, indexRouter);
+app.use(`${STATIC_BASE}/points`, pointsRouter);
+app.use(`${STATIC_BASE}/isochrone`, isochroneRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
