d50610ae46858b797a5f87509fe5d1002fb754a3
Christian Engel <ch-engel@posteo.de>
2022-09-21 15:34:42 +0200

    Add isochrone config

--- routes/isochrone.js
+++ routes/isochrone.js
@@ -6,17 +6,27 @@ const dissolve = require("@turf/dissolve");
 const union = require("@turf/union");
 const difference = require("@turf/difference");
 const polygonSmooth = require("@turf/polygon-smooth");
+const colorbrewer = require("colorbrewer");
 
 const router = express.Router();
 
-const BUCKETS = 5;
+let buckets, timeLimit, colors;
 const GRAPHHOPPER_ISOCHRONE_URL =
   process.env.GRAPHHOPPER_ISOCHRONE_URL || "http://localhost:8080/isochrone";
 
 router.post("/", async (req, res, next) => {
   try {
-    const points = req.body;
-    const rawGeoms = await getRawGeometries(points);
+    const points = req.body.points;
+    timeLimit = req.body.time_limit * 60;
+    buckets = parseInt(req.body.buckets);
+    if (buckets === 2) {
+      colors = [colorbrewer.PiYG[3][0], colorbrewer.PiYG[3][2]];
+    } else if (buckets === 1) {
+      colors = [colorbrewer.PiYG[3][2]];
+    } else {
+      colors = colorbrewer.PiYG[buckets].reverse();
+    }
+    const rawGeoms = await getRawGeometries(points, timeLimit, buckets);
     const smoothedGeoms = smoothGeoms(rawGeoms);
     const isochrones = getIsochrones(smoothedGeoms);
     res.send(isochrones);
@@ -25,11 +35,15 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-const getRawGeometries = async (points) => {
+const getRawGeometries = async (points, timeLimit, buckets) => {
   let geoms = [];
   for (const feature of points.features) {
     if (feature.geometry) {
-      const apiResult = await fetchApi(feature.geometry.coordinates);
+      const apiResult = await fetchApi(
+        feature.geometry.coordinates,
+        timeLimit,
+        buckets
+      );
       if (apiResult.polygons) {
         for (const polygon of apiResult.polygons) {
           geoms.push(polygon);
@@ -40,9 +54,9 @@ const getRawGeometries = async (points) => {
   return geoms;
 };
 
-const fetchApi = async (point) => {
+const fetchApi = async (point, timeLimit, buckets) => {
   const coords = point.reverse().join(",");
-  const url = `${GRAPHHOPPER_ISOCHRONE_URL}?point=${coords}&profile=foot&time_limit=600&buckets=${BUCKETS}`;
+  const url = `${GRAPHHOPPER_ISOCHRONE_URL}?point=${coords}&profile=foot&time_limit=${timeLimit}&buckets=${buckets}`;
 
   const result = await axios.get(url);
   return await result.data;
@@ -63,7 +77,7 @@ const getIsochrones = (polygons) => {
 const unionFeatures = (featureCollection) => {
   let unionedFeatures = [];
   let unionedFeature;
-  for (let i = 0; i < BUCKETS; i++) {
+  for (let i = 0; i < buckets; i++) {
     unionedFeature = null;
     for (const feature of featureCollection.features) {
       if (i === feature.properties.bucket) {
@@ -74,6 +88,7 @@ const unionFeatures = (featureCollection) => {
       }
     }
     unionedFeature.properties.bucket = i;
+    unionedFeature.properties.color = colors[i];
     unionedFeatures.push(unionedFeature);
   }
   return unionedFeatures;
